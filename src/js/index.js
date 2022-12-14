70d2d651605699b9403cf0c94b4eef7b15672a65
petrovskya <petrovska.ya@icloud.com>
2022-12-15 00:23:32 +0300

    added changes

--- src/js/index.js
+++ src/js/index.js
@@ -14,29 +14,28 @@ function toFullDate(day, month, year) {
 let currentDate = document.querySelector('.date');
 currentDate.textContent = toFullDate(day, month, year);
 
-
-
-
-
-
-
 const mainWrap = document.querySelector('.main-wrap');
 const taskBtnAdd = document.querySelector('.task-btn-add');
-const cardContent = document.querySelectorAll('.card-content');
+const cardContentToDo = document.querySelector('.card-content__to-do');
+const cardContentIn = document.querySelector('.card-content__in');
+const cardContentDone = document.querySelector('.card-content__done');
 const newTaskWrap = document.querySelector('.new-task-wrap');
 const cardCloseBtn = document.querySelector('.card-close-btn');
 const cancelBtn = document.querySelector('.task-btn-cancel');
 const deleteAllBtn = document.querySelectorAll('.task-btn-delete-all');
-
-
-
-
 
 let newTaskTitle = document.querySelector('.new-task-title');
 let newTaskDesc = document.querySelector('.new-task-desc');
 let newTaskAuthor = document.querySelector('.new-task-author');
 
 let totalToDo = document.querySelectorAll('.score');
+function countTotal(status, arr){
+  totalToDo.forEach((item) => {
+    if(item.dataset.status == status){
+      item.innerHTML = arr.length;
+    }
+  });
+}
 
 const toDo = [];
 const inProgress = [];
@@ -59,9 +58,9 @@ let Task = function(title, desc, author, status, date) {
   this.date = date;
 }
 
-function render(task){
+function render(task, idx){
   return `
-    <div class="task" data-status="toDo">
+    <div class="task" data-status="toDo" id="${idx}">
       <div class="task-header">
         <span class="task-status">${task.status}</span>
         <p class="task-date">${task.date}</p>
@@ -70,10 +69,10 @@ function render(task){
       <div class="task-desc">${task.desc}</div>
       <div class="task-author">${task.author}</div>
       <div class="task-options">
-        <button class="task-btn-move-on">
+        <button class="task-btn-move-on" id="${idx}">
           <i class="fa-regular fa-circle-right"></i>
         </button>
-        <button class="task-btn-delete">
+        <button class="task-btn-delete" id="${idx}">
           <i class="fa-solid fa-trash-can"></i>
         </button>
       </div>            
@@ -81,23 +80,82 @@ function render(task){
   `  
 }
 
+function outToDo(){
+  cardContentToDo.innerHTML = '';
+  toDo.forEach((task, idx) => cardContentToDo.innerHTML += render(task, idx));
+}
+function outInProgress(){
+  cardContentIn.innerHTML = '';
+  inProgress.forEach((task, idx) => cardContentIn.innerHTML += render(task, idx));
+}
+function outDone(){
+  cardContentDone.innerHTML = '';
+  done.forEach((task, idx) => cardContentDone.innerHTML += render(task, idx));
+}
+
+
 const createTask = function() {
   let taskDate = toFullDate(day, month, year);
-  let status = 'toDo';
-  let newTask = new Task(newTaskTitle.value, newTaskDesc.value, newTaskAuthor.value, status, taskDate);
-  cardContent.forEach((item) => {
-    if(item.dataset.status == 'toDo'){
-      item.innerHTML += render(newTask);
-    }
-  });
+  let newTask = new Task(newTaskTitle.value, newTaskDesc.value, newTaskAuthor.value, 'toDo', taskDate);
   toDo.push(newTask);
-  console.log(toDo);
-  totalToDo.forEach((item) => {
-    if(item.dataset.status == 'toDo'){
-      item.innerHTML = toDo.length;
-    };
-  });
+  outToDo();
+  countTotal('toDo', toDo);
   clearPopUp();
+  
+  let moveOnBtns = document.querySelectorAll('.task-btn-move-on'); 
+  let deleteTaskBtns = document.querySelectorAll('.task-btn-delete'); 
+  deleteTaskBtns.forEach((item) => {
+    item.addEventListener('click', deleteTask);
+  });
+  moveOnBtns.forEach((item) => {
+    item.addEventListener('click', moveOnTask);
+  });
+}
+
+function deleteTask(e){
+  let thisTask = e.currentTarget.closest('div[data-status]');
+  toDo.splice(thisTask, 1);
+  thisTask.remove();
+  let taskDivs = document.querySelectorAll('.task');
+  let deleteTaskBtns = document.querySelectorAll('.task-btn-delete'); 
+  let moveOnBtns = document.querySelectorAll('.task-btn-move-on'); 
+  taskDivs.forEach((item,idx) => {
+    item.setAttribute('id', idx);
+  });
+  deleteTaskBtns.forEach((btn, idx) => {
+    btn.setAttribute('id', idx);
+  });
+  moveOnBtns.forEach((btn, idx) => {
+    btn.setAttribute('id', idx);
+  });
+  countTotal('toDo', toDo);
+}
+function moveOnTask(e){
+
+  let thisTask = e.currentTarget.closest('div[data-status]');
+  let id = thisTask.id;
+  console.log(id);
+  inProgress.push(toDo[id]);
+  console.log(inProgress);
+  toDo.splice(id, 1);
+  console.log(toDo);
+  thisTask.remove();
+  let taskDivs = document.querySelectorAll('.task');
+  let deleteTaskBtns = document.querySelectorAll('.task-btn-delete'); 
+  let moveOnBtns = document.querySelectorAll('.task-btn-move-on'); 
+  taskDivs.forEach((item,idx) => {
+    item.setAttribute('id', idx);
+  });
+  deleteTaskBtns.forEach((btn, idx) => {
+    btn.setAttribute('id', idx);
+  });
+  moveOnBtns.forEach((btn, idx) => {
+    btn.setAttribute('id', idx);
+  });
+  countTotal('toDo', toDo);
+  countTotal('inProgress', inProgress);
+  outInProgress();
+
 }
 
 taskBtnAdd.addEventListener('click', () => newTaskWrap.classList.add('active'));
@@ -106,9 +164,7 @@ cardCloseBtn.addEventListener('click', function(){
   newTaskWrap.classList.toggle('active');
 });
 
-cancelBtn.addEventListener('click', () => {
-  clearPopUp();
-});
+cancelBtn.addEventListener('click', clearPopUp());
 
 const taskBtnNew = document.querySelector('.task-btn-new');
 taskBtnNew.addEventListener('click', createTask);
@@ -121,11 +177,9 @@ function deleteTasks(status, arr){
       item.remove();
     }
   });
-  totalToDo.forEach((item) => {
-    if(item.dataset.status == status){
-      item.innerHTML = arr.length;
-    };
-  });
+  countTotal('toDo', toDo);
+  countTotal('inProgress', inProgress);
+  countTotal('done', done);
 }
 deleteAllBtn.forEach((item) => {
   item.addEventListener('click', (e) => {
@@ -135,7 +189,7 @@ deleteAllBtn.forEach((item) => {
       deleteTasks('inProgress', inProgress);
     } else if(e.currentTarget.dataset.status == 'done'){
       deleteTasks('done', done);
-    };
+    }
   });
 });
 
